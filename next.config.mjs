b318e824730b4f4ce164cc9cbbe997647261e9ec
nhmint19 <nguyenhoangminh191202@gmail.com>
2024-08-01 02:56:10 +1000

    cors errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,33 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Apply headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "blogging-app-rose-eight.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
